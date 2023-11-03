c5ec74288976005fc089e48a31d7276563edc004
jadelcruz1 <jardelcruz6@gmail.com>
2023-11-02 23:07:58 -0300

    Controller finalizado

--- src/main/java/br/com/kafka/messagingspringboot/MessagingSpringbootApplication.java
+++ src/main/java/br/com/kafka/messagingspringboot/MessagingSpringbootApplication.java
@@ -2,6 +2,10 @@ package br.com.kafka.messagingspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MessagingSpringbootApplication {
@@ -10,4 +14,19 @@ public class MessagingSpringbootApplication {
 		SpringApplication.run(MessagingSpringbootApplication.class, args);
 	}
 
+	@RestController // controlador rest para realizar a chamada.
+	@RequestMapping("/kafka") // endpoint para produzir a mensagem.
+	class HelloController{
+		private HelloProducer service;
+
+		public HelloController(HelloProducer service) {
+			this.service = service;
+		}
+		@GetMapping("/hello/{name}")
+		public String hello(@PathVariable("name") String name){
+			service.sendMessage("Hello, " + name);
+			return "ok";
+		}
+
+	}
 }
